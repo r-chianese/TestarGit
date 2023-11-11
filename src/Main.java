c8f130a4b8d3c628935508b7fdadad7dbf3897ff
rchianese <r.chianese@infinera.com>
2023-11-11 22:58:18 +0000

    Sexta commit

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.printf("Hello and welcome!22");
         System.out.printf("Hello and welcome!333");
         System.out.printf("Hello and welcome!444");
+        System.out.printf("Hello and welcome!555");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
